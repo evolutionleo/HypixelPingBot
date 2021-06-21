fe0b357f6b1680361aa4605aa5ffeb7f96c7d69d
Emirchik <tah116emir@outlook.com>
2021-06-21 21:23:05 +0300

    копипаст кода левы

--- bot.js
+++ bot.js
@@ -2,13 +2,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('token.json').token;
 
+const prefix = "#";
+
 function handler() {
     console.log('ready! ass! dick!')
 }
 
 function message_handler(message) {
-    if (message.content === 'ping') {
-        message.reply('pong');
+    if (message.content.starsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command == "hypixel_refresh") {
+        // TODO сюда подключить скрип
     }
 }
 
