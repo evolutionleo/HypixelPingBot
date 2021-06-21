a88f9faf4d248f0b6dbc9923a9a44f9019b4471c
Evoleo <evolutionleo2016@gmail.com>
2021-06-21 20:19:58 +0300

    ass dick cock
    e

--- bot.js
+++ bot.js
@@ -2,15 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = require('token.json').token;
 
+function handler() {
+    console.log('ready! ass! dick!')
+}
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+function message_handler(message) {
+    if (message.content === 'ping') {
+        message.reply('pong');
     }
-});
+}
+
+client.on('ready', handler);
+client.on('message', message_handler);
+
 
 client.login(token);
